a5bbfa154622a9101349dea6fbcb739152d03c34
Kev Morel <kev.morel@northcoders.com>
2022-02-26 17:38:32 +0000

    files copied into new react app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { UserProvider } from './contexts/UserContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
